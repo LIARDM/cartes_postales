a9e7f5090ff84282aa4148e784339751e73f4e29
Furie_Celeste <mevenliard@gmail.com>
2020-05-30 13:44:19 +0200

    Finalement, index.php me semble plus intéressant... (tests)

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Cartes Postales</title>
+    </head>
+
+    <body>
+        <?php
+        echo "Bonjour";
+        $_sayMyName = "Meven";
+        
+        ?>
+        <h1>Bonjour <?php $_sayMyName ?></h1>
+        
+
+
+    </body>
+
+</html>
