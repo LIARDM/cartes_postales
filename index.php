2482c35ad03f76df879cb9c6dc89ef479985e38f
Furie_Celeste <mevenliard@gmail.com>
2020-10-10 16:13:15 +0200

    mise en forme page d'accueil

--- index.php
+++ index.php
@@ -7,19 +7,31 @@
         <meta name="author" content="LIARD Meven">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="./css/style_default.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <title>Cartes Postales</title>
     </head>
 
-    <body>
-    <header>
-        <h1 id='titre'>Bienvenue sur ma Collection de Cartes Postales</h1>
-        <?php 
-        include('bdd_cp.inc.php');
-        ?>
-    </header>
+    <body id='fond'>
+        <div id='container'>
+            <nav class="navbar navbar-expend-lg navbar-light">
+                <a class=navbar-nav href="#">Accueil</a>
+                <a class=navbar-nav href="#">Par Pays</a>
+                <a class=navbar-nav href="#">Par Expediteur</a>
+                <a class=navbar-nav href="#">Par Thème</a>
+                <a class=navbar-nav href="#">Contact</a>
+            </nav>
+
+            <header>
+                <h1 id='titre'>Bienvenue sur ma Collection de Cartes Postales</h1>
+                <img id="index_img-1" src="/img/index_img-1.png"></img>
+                <?php 
+                include('bdd_cp.inc.php');
+                ?>
+            </header>
+        </div>
         
-
-
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     </body>
-    <footer></footer>    
 </html>
