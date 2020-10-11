6f3c84074b827456fa751b6aa04e8d772e9b99c7
Furie_Celeste <mevenliard@gmail.com>
2020-10-11 22:47:11 +0200

    Mise en forme page d'accueil

--- index.php
+++ index.php
@@ -25,9 +25,29 @@
                 <h1 id='titre'>Bienvenue sur ma Collection de Cartes Postales</h1>
                 <img id="index_img-1" src="/img/index_img-1.png"></img>
                 <?php 
-                include('bdd_cp.inc.php');
+
+                // include('bdd_cp.inc.php');
                 ?>
             </header>
+
+            <section id="section">
+                <aside id="bloc_phare">
+                    <?php
+                        setlocale(LC_ALL, 'fr_FR');
+                        
+                        echo "<p>Nous sommes le : " . strftime("%d %B %Y")."</p>";                        ;
+                    
+                    ?>
+                    <img src="/img/les-pierres-noires_1_lmresized_1.jpg"></img>
+                </aside>
+                <article id="bloc_presentation">
+                    <h4>Présentation</h4>
+                    <p>Bonjour à vous qui arrivez sur ce site de ma collection de cartes postales.<br>
+                        J'ai voulu que chacun puisse voir et lire les cartes que j'ai reçu , et celles que Maman avait gardée.<br>
+                        Qu'ils en soient ici chaleureusement remerciés.<br>
+                        J'espère que vous continuerez à faire partager ainsi vos émotions de voyages.</p>
+                </article>
+            </section>
         </div>
         
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
